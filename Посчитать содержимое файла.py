85502c8b52d72f08d34c6a926f8d19f7c9bd6e88
Pavel2207 <88669757+Pavel2207@users.noreply.github.com>
2021-09-08 09:22:53 +0300

    Задачи со Степика

---
+++ Посчитать содержимое файла.py
@@ -0,0 +1,12 @@
+f=open(r'C:\Users\777\Downloads\numbers.txt')
+d=[]
+s=[]
+for i in f:
+    i=str(i)
+    if len(i)==4:
+        d.append(i)
+    if len(i)==3:
+        s.append(int(i))
+print(len(d))
+print(sum(s))
+f.close()
